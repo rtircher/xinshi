98f6bae6c05de38b3ca78efeddd3a0cd87f3550f
Renaud Tircher <renaud.tircher@gmail.com>
2012-10-07 22:48:24 -0700

    Adding option to json response to specify the http status

--- src/xinshi/response.clj
+++ src/xinshi/response.clj
@@ -1,11 +1,14 @@
 (ns xinshi.response
-  (:use [ring.util.response :only (response content-type)])
+  (:use [ring.util.response :only (response content-type status)])
   (:require [clj-json.core :as json]))
 
 (defn html [data]
   (-> (response data)
       (content-type "text/html; charset=utf-8")))
 
-(defn json [data]
-  (-> (response (json/generate-string data))
-      (content-type "application/json")))
+(defn json
+  ([data] (json data 200))
+  ([data http-status]
+     (-> (response (json/generate-string data))
+         (content-type "application/json")
+         (status http-status))))
